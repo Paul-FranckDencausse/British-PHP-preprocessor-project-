9b597cc9b7be5f5428e2a0e75ea27f619fa69af5
Paul-Franck Dencausse <121952248+Paul-FranckDencausse@users.noreply.github.com>
2025-09-10 20:01:10 +0000

    vocabulaire britannique

--- preprocessor.php
+++ preprocessor.php
@@ -8,11 +8,28 @@ $sourceCode = file_get_contents($sourceFile);
 // 2. Transformer les variables '£' en '$'
 $processedCode = str_replace('£', '$', $sourceCode);
 
-// 3. Sauvegarder dans un fichier PHP temporaire
+// 3. Remplacer les mots britanniques par leur équivalent américain
+$britishToAmerican = [
+    'colour'    => 'color',
+    'favourite' => 'favorite',
+    'organise'  => 'organize',
+    'initialise'=> 'initialize',
+    'analyse'   => 'analyze',
+    'behaviour' => 'behavior',
+    'centre'    => 'center',
+    'metre'     => 'meter',
+    'licence'   => 'license',
+    'defence'   => 'defense',
+    // Ajoute d'autres mots si besoin
+];
+
+$processedCode = str_ireplace(array_keys($britishToAmerican), array_values($britishToAmerican), $processedCode);
+
+// 4. Sauvegarder dans un fichier PHP temporaire
 $targetFile = 'mon_code_translated.php';
 file_put_contents($targetFile, $processedCode);
 
-// 4. Exécuter le fichier PHP traduit
+// 5. Exécuter le fichier PHP traduit
 include $targetFile;
 
 // Optionnel : Supprimer le fichier temporaire après exécution
